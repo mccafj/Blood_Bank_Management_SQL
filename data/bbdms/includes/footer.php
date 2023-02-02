176848482ddb60153ab61bb4f333de7eeb9e3fb5
Jonathan McCaffrey <jonmcjon@hotmail.com>
2023-02-02 17:42:15 -0500

    totally changed project.  Ha

--- data/bbdms/includes/footer.php
+++
@@ -1,8 +0,0 @@
-  
-  
-  <footer class="py-5 bg-inverse">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; BloodBank & Donor Management System 2017</p>
-        </div>
-        <!-- /.container -->
-    </footer>
